1eccac0fe504c142718650f738f32519f4046082
jlee335 <21jlee335@gmail.com>
2020-01-05 05:25:39 +0900

    Image Sharing with Server Complete!

--- app/src/main/java/com/example/project1/Gallery/MyAdapter.java
+++ app/src/main/java/com/example/project1/Gallery/MyAdapter.java
@@ -28,7 +28,7 @@ public class MyAdapter extends BaseAdapter
     private MyApplication app;
     private int layout;
     LayoutInflater inf;
-    String url = "http://cs497madcampproj2yeah.localtunnel.me/image";
+    String url = "https://27aa638c.ngrok.io/image";
 
 
     public MyAdapter(int layout, Context mContext)
