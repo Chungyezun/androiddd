56d18a9083710d00f9f2b9eb7282cfa6b7d8eb42
Chungyezun <maple0729@kaist.ac.kr>
2020-01-04 22:16:13 +0900

    facebook 연동 오류 해결

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.project1;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 
@@ -32,6 +36,8 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -108,6 +114,24 @@ public class MainActivity extends AppCompatActivity {
         app = (MyApplication)this.getApplicationContext();
 
         setContentView(R.layout.login); //첫 화면은 login.xml 로
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.project1",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+        }
+        catch (NoSuchAlgorithmException e) {
+        }
+
+
+
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -199,6 +223,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     protected void onResume(){
         super.onResume();
@@ -216,11 +245,7 @@ public class MainActivity extends AppCompatActivity {
         Activity currAct = app.getCurrentActivity();
         if(this.equals(currAct))app.setCurrentActivity(null);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+
 
 
 }
