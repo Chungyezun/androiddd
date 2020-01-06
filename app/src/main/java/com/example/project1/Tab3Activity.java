ff2a2e0b4279f67ab8885530b510b01273936e10
Chungyezun <maple0729@kaist.ac.kr>
2020-01-07 04:23:53 +0900

    Merge branch 'UI_Hyunjae' of https://github.com/Chungyezun/androiddd into UI_Hyunjae
    # Conflicts:
    #	app/src/main/java/com/example/project1/IOcustom.java

--- app/src/main/java/com/example/project1/Tab3Activity.java
+++ app/src/main/java/com/example/project1/Tab3Activity.java
@@ -7,7 +7,11 @@ import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
@@ -28,7 +32,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.util.Pair;
 
-import com.example.project1.Game.Player;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -38,21 +41,18 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -61,31 +61,7 @@ public class Tab3Activity extends AppCompatActivity implements GoogleMap.OnMapCl
 
     private GoogleMap mGoogleMap;
     private MyApplication app;
-    List<CircleOptions>enemies;
-    CircleOptions battleRadius;
-    List<Circle>enemyCircle;
-    List<GoogleMap.OnCircleClickListener> circleClickListeners;
-    Circle myCircle;
-    Thread t = new Thread(){
-        @Override
-        public void run(){
-            while(true){
-                try {
-                    Thread.sleep(1000);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            update();
-                            Log.d("t","tick");
-                        }
-                    });
-                }catch(InterruptedException e){
-                    break;
-                }
-            }
-        }
 
-    };
 
 
     @Override
@@ -103,82 +79,7 @@ public class Tab3Activity extends AppCompatActivity implements GoogleMap.OnMapCl
         mGoogleMap = googleMap;
         //지도타입 - 일반
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        battleRadius = new CircleOptions()
-            .center( new LatLng(app.getMyPlayer().getLocation().first,app.getMyPlayer().getLocation().second))
-                .radius(50)
-                .fillColor(Color.parseColor("#880000ff"))
-                .strokeWidth(0f)
-                .clickable(false);
-        myCircle = mGoogleMap.addCircle(battleRadius);
-
-        int length;
-        if(app.getAllPlayers() == null) {
-            length = 0;
-        }else{
-            length =app.getAllPlayers().size();
-        }
-
-        enemies = new ArrayList<>();
-        enemyCircle = new ArrayList<>();
-
-        List<Player> allPlayers = app.getAllPlayers();
-        if(allPlayers == null){
-
-        }else{
-            for(int i = 0; i < length;i++){
-                if(!app.getAllPlayers().get(i).getName().equals(app.getMyPlayer().getName()) ){
-                    CircleOptions otherCircles = new CircleOptions()
-                            .center( new LatLng(app.getAllPlayers().get(i).getLocation().first,app.getAllPlayers().get(i).getLocation().second))
-                            .radius(5)
-                            .fillColor(Color.parseColor("#8800ff00"))
-                            .clickable(true);
-                    enemies.add(otherCircles);
-                }
-            }
-        }
-        for(int i = 0; i < length;i++){
-            if(!app.getAllPlayers().get(i).getName().equals(app.getMyPlayer().getName()) ){
-                enemyCircle.add(mGoogleMap.addCircle(enemies.get(i)));
-                enemyCircle.get(enemyCircle.size()-1).setTag(app.getAllPlayers().get(i).getName());
-            }else{
-
-                Log.d("me","me");
-            }
-        }
-
-        mGoogleMap.setMyLocationEnabled(true);
-        mGoogleMap.setOnMyLocationButtonClickListener(null);
-        mGoogleMap.setOnMyLocationClickListener(null);
-        mGoogleMap.setOnCircleClickListener(new GoogleMap.OnCircleClickListener() {
-            @Override
-            public void onCircleClick(Circle circle) {
-                String name = (String) circle.getTag();
-                Log.d("SELECT_NAME",name);
-
-            }
-        });
         init();
-
-
-
-    }
-    public void update(){
-        int length;
-        if(app.getAllPlayers() == null) {
-            length = 0;
-        }else{
-            length =app.getAllPlayers().size()-1;
-        }
-
-        List<Player> allPlayers = app.getAllPlayers();
-        if(allPlayers == null){
-        }else {
-            for (int i = 0; i < length; i++) {
-                enemyCircle.get(i).setCenter(new LatLng(app.getAllPlayers().get(i).getLocation().first, app.getAllPlayers().get(i).getLocation().second));
-            }
-        }
-        myCircle.setCenter(new LatLng(app.getMyPlayer().getLocation().first,app.getMyPlayer().getLocation().second));
-
     }
 
 
@@ -222,7 +123,7 @@ public class Tab3Activity extends AppCompatActivity implements GoogleMap.OnMapCl
             mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
             // Map 을 zoom 합니다.
-            mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+            mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
 
             // 마커 설정.
             MarkerOptions optFirst = new MarkerOptions();
@@ -230,6 +131,11 @@ public class Tab3Activity extends AppCompatActivity implements GoogleMap.OnMapCl
             optFirst.title("Current Position");// 제목 미리보기
             optFirst.snippet("Snippet");
             mGoogleMap.addMarker(optFirst).showInfoWindow();
-            t.start();
+            CircleOptions circle1KM = new CircleOptions().center(latLng)
+                    .radius(1000)
+                    .strokeWidth(0f)
+                    .fillColor(Color.parseColor("#880000ff"));
+            mGoogleMap.addCircle(circle1KM);
     }
+
 }
